575902c91ec2b5e10e1025421856547a57b51d39
DJ <drj613@gmail.com>
2020-04-18 18:02:06 -0700

    initializing repo

---
+++ 01_the-vue-instance/main.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function(event) {
+  let product = "Socks";
+
+  let app = new Vue({
+    el: "#app",
+    data: {
+      product: 'Socks',
+      description: 'A pair of warm, fuzzy socks'
+    }
+  })
+
+});
