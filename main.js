a03a45a63156406149b8904a11ed8f709f6daaf3
DJ <drj613@gmail.com>
2020-04-24 12:13:20 -0700

    finished ch4 - list rendering

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ window.addEventListener("load", function(event) {
     data: {
       product: 'Socks',
       description: 'A pair of warm, fuzzy socks',
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "Green"
+        },
+        {
+          variantId: 2235,
+          variantColor: "Blue"
+        }
+      ],
+      sizes: ["Small", "Medium", "Large"],
       image: "./assets/vm-socks-green.png",
       altText: "A pair of socks",
       inventory: 100,
